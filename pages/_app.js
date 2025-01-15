819b19461e2ee55baadf8b7dc4033d8d55d1ce33
Zevik <zaviner@gmail.com>
2025-01-15 10:47:34 +0200

    Fix horizontal scrolling issues

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div dir="rtl" className="min-h-screen bg-gray-100">
+    <div dir="rtl" className="min-h-screen bg-gray-100 overflow-x-hidden">
       <Component {...pageProps} />
     </div>
   )
