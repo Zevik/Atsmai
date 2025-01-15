570a4b8c8c39c53de848ebef6ac6d84b4283930e
Zevik <zaviner@gmail.com>
2025-01-15 09:30:42 +0200

    Fix Tailwind CSS configuration and styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div dir="rtl" className="min-h-screen bg-gray-100">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      direction: ['rtl'],
+    },
   },
   plugins: [],
 }
