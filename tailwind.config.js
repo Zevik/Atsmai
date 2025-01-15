f56c19bb90782f7c28199d118be3b3cd9ca0b95d
Zevik <zaviner@gmail.com>
2025-01-15 09:48:15 +0200

    Fix CSS and Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      direction: ['rtl'],
       colors: {
         primary: '#60BFC1',
         secondary: '#BADFD7',
