69527c6af9aed7932941c75a9296887b679a62af
Zevik <zaviner@gmail.com>
2025-01-15 12:05:46 +0200

    Add simple header and footer with WhatsApp link

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div dir="rtl" className="min-h-screen bg-gray-100 overflow-x-hidden">
-      <Component {...pageProps} />
+    <div dir="rtl" className="min-h-screen overflow-x-hidden">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   )
 }
