87fbd6ce7547cafb76b4afa6c3bb63a2339df674
Zevik <zaviner@gmail.com>
2025-01-19 15:34:39 +0200

    Adjust top padding and update headings on homepage

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,10 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-secondary/20 overflow-x-hidden p-4 flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-primary animate-bounce mb-4">
-          עצמאים ועצמאיות בישראל
-        </h1>
-        <p className="text-lg text-gray-600">
-          כלמיני דברים שרציתם לדעת
-        </p>
+    <div className="min-h-screen bg-secondary/20 overflow-x-hidden p-4">
+      <div className="w-full max-w-4xl mx-auto bg-white p-2 md:p-8 rounded-lg shadow-lg overflow-hidden">
+        <h1 className="text-3xl font-bold mb-4 text-primary text-center">עצמאים ועצמאיות בישראל</h1>
+        <h2 className="text-xl font-semibold mb-6 text-center">כלמיני דברים שרציתם לדעת</h2>
       </div>
     </div>
   );
