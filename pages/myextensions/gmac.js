a65b2e512c0e5c0d2b673948ca78449b6aa28d81
Zevik <zaviner@gmail.com>
2025-01-26 21:23:39 +0200

    עדכון קבצים בתיקייה

--- pages/myextensions/gmac.js
+++ pages/myextensions/gmac.js
@@ -1,13 +1,10 @@
-
 import React from 'react';
 
 export default function Gmac() {
   return (
-    <Layout>
-      <section>
-        <h1 className="text-3xl font-bold mb-4">Gmac</h1>
-        <p className="mb-6">זהו עמוד תיאור למתוסף Gmac. כאן תוכל לספק מידע נוסף על הפונקציות והיתרונות של המתוסף.</p>
-      </section>
-    </Layout>
+    <section>
+      <h1 className="text-3xl font-bold mb-4">Gmac</h1>
+      <p className="mb-6">זהו עמוד תיאור למתוסף Gmac. כאן תוכל לספק מידע נוסף על הפונקציות והיתרונות של המתוסף.</p>
+    </section>
   );
 }
